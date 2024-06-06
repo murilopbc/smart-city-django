00f52fe96f87c95612ae9384dce9eaf6b1405aed
murilopbc <murilopoltronieri3@gmail.com>
2024-06-06 16:53:35 -0300

    load_sensores

--- app_smart/api/viewsets.py
+++ app_smart/api/viewsets.py
@@ -3,24 +3,53 @@ from rest_framework import generics, permissions
 from app_smart.api import serializers
 from ..models import Sensor
 from rest_framework import viewsets
-
 from app_smart.api.filters import SensorFilter
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.views import APIView
+from django.db.models import Q
+from rest_framework.routers import Response
 
 
-class CreateUserApiViewSet(generics.CreateAPIView):
+class CreateUserAPIViewSet(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
-    #permissions_classes = [permission.IsAdminUser]
+    # permission_classes = [permissions.IsAdminUser]
 
-
-    def post(self,request,*args, **kwargs):
-        return self.create(request,*args,**kwargs)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs )
     
+
 class SensorViewSet(viewsets.ModelViewSet):
     queryset = Sensor.objects.all()
     serializer_class = serializers.SensorSerializer
-    permissions_class = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated]
     filter_backends = [DjangoFilterBackend]
     filterset_class = SensorFilter
-    
+
+class SensorFilterView (APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        tipo = request.data.get('tipo', None)
+        localizacao = request.data.get('localizacao', None)
+        responsavel = request.data.get('responsavel', None)
+        status_operacional = request.data.get('status_operacional', None)
+
+
+        filters = Q()
+        if tipo: 
+            filters &=Q(tipo__icontains=tipo)
+        
+        if localizacao:
+             filters &= Q(localizacao__icontains=localizacao)
+
+        if responsavel:
+            filters &= Q(responsavel__icontains=responsavel)
+
+        if status_operacional:
+            filters &= Q(status_operacional__icontains=status_operacional)
+
+        queryset = Sensor.objects.filter(filters)
+        serializer = serializers.SensorSerializer(queryset, many=True)
+        return Response(serializer.data)
+
--- app_smart/urls.py
+++ app_smart/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from . import views
 from django.contrib import admin
 from django.urls import path, include
-from app_smart.api.viewsets import  CreateUserApiViewSet, SensorViewSet
+from app_smart.api.viewsets import  CreateUserApiViewSet, SensorFilterView, SensorViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.routers import DefaultRouter
 
@@ -30,4 +30,6 @@ urlpatterns = [
     path('api/token', TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path('api/token/refresh', TokenRefreshView.as_view(), name="token_refresh"),
     path('api/',include(router.urls)), 
+     path('api/sensor_filter/', SensorFilterView.as_view(),name='sensor_filter' )
 ]
+
