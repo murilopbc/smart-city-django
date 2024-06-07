b272a31ad1bae8aedda4eeb906efe2d23cce389f
murilopbc <murilopoltronieri3@gmail.com>
2024-06-07 08:58:57 -0300

    finalizado

--- smarty_city/settings.py
+++ smarty_city/settings.py
@@ -53,7 +53,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME':timedelta(minutes=15),
+    'ACCESS_TOKEN_LIFETIME':timedelta(minutes=30),
     'REFRESH_TOKEN_LIFETIME':timedelta(days=7),
 }
 
